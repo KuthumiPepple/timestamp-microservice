b0d0a14b5514e76bfa154a5daa2cf93de2528ea9
Kuthumi Pepple <kuthumipepple@gmail.com>
2021-03-08 15:50:43 +0100

    ft: implement all user stories

--- server.js
+++ server.js
@@ -6,27 +6,41 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
 app.get('/api/timestamp/:date?', (req, res) => {
-  res.json({unix: Date.parse(req.params.date)})
-})
+  let input = req.params.date;
+  // if input is null, use current date
+  if (input == null) {
+    let date = new Date();
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+    return;
+  }
+  // if input is timestamp, convert type to Integer
+  if (/^\d{5,}$/.test(input)) input = parseInt(input, 10);
+  
+  const date = new Date(input);
+  if (!/Invalid Date/i.test(date.toUTCString())) {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  } else {
+    res.json({ error: 'Invalid Date' });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
